3b5b68811bdcfe565543f1acb9c2689912984f91
chandan-m <chandanmanjunath24@gmail.com>
2025-12-03 16:49:57 -0800

    Cleanup Preferences Modal

--- frontend/src/components/modals/UserPreferencesModal.tsx
+++ frontend/src/components/modals/UserPreferencesModal.tsx
@@ -5,7 +5,8 @@ import {
   Group,
   Stack,
   Textarea,
-  TextInput,
+  TextInput, ComboboxItem,
+  NumberInput,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,6 +15,12 @@ import { setUserPreferences } from "../../store/userSlice";
 import { UserPreferences } from "@common/types/account";
 import { updateUserPreferences } from "../../services/account.service";
 import { setUserCookie, getTokenFromCookie } from "../../utils/authCookies";
+import {env} from "../../config/env";
+import {useQuery} from "@tanstack/react-query";
+import {queryKeys} from "../../lib/queryKeys";
+import {apiClient} from "../../lib/apiClient";
+import {OllamaModel} from "@common/types/ollama";
+import {useMemo} from "react";
 
 interface Props {
   opened: boolean;
@@ -24,10 +31,22 @@ export default function UserPreferencesModal({ opened, onClose }: Props) {
   const dispatch = useDispatch();
   const user = useSelector((s: RootState) => s.user.user);
 
+  const {data: models} = useQuery({
+    queryKey: queryKeys.models.all,
+    queryFn: () => apiClient.get<OllamaModel[]>(
+      "/api/chat/models"
+    ).then(res => res.data)
+  })
+  const modelOptions: ComboboxItem[] = useMemo(() => {
+      return models?.map(model => ({value: model.name, label: model.name})) ?? []
+    },
+    [models]
+  )
+
   const form = useForm<UserPreferences>({
     initialValues: {
-      model: user?.preferences?.model ?? "gemma3:270m",
-      temp: user?.preferences?.temp ?? 0.7,
+      model: user?.preferences?.model ?? env.defaultModel,
+      temp: user?.preferences?.temp ?? env.defaultTemperature,
       theme: user?.preferences?.theme ?? "light",
       custom_instructions: user?.preferences?.custom_instructions ?? "",
     },
@@ -62,22 +81,24 @@ export default function UserPreferencesModal({ opened, onClose }: Props) {
 
           <Select
             label="Preferred model"
-            data={[
-              { value: "gemma3:270m", label: "Gemma3:270M" },
-              { value: "qwen3:0.6b", label: "Qwen3:0.6b" },
-            ]}
+            data={modelOptions}
             {...form.getInputProps("model")}
           />
 
-          <TextInput
+          <NumberInput
             label="Default Model Temperature"
-            type="number"
+            min={0.0}
+            max={2.0}
+            step={0.1}
+            decimalScale={1}
             {...form.getInputProps("temp")}
           />
 
           <Textarea
             label="Custom instructions"
             placeholder="Enter custom instructions used for all models."
+            autosize
+            minRows={5}
             {...form.getInputProps("custom_instructions")}
           />
 
